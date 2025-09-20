b04b630d10adaef3659dba07178d3c4e7fb43ffb
athikapriya <athikapriya1997@gmail.com>
2025-09-20 23:15:02 +0600

    add URL path for user registration page

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . views import TaskList, TaskDetail, TaskCreate, UpdateTask, DeleteTask, CustomLoginView
+from . views import TaskList, TaskDetail, TaskCreate, UpdateTask, DeleteTask, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name = 'login'), # for user login
     path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'), # for user logout
+    path('register/', RegisterPage.as_view(), name = 'register'), # for user registration
 
     path('', TaskList.as_view(), name = 'tasks'),   # home page showing list of tasks
     path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'), # detail view for a specific task identified by its primary key (pk)
